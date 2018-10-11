5e8ec87ed1399be4de292243134c1e4c5db125f6
Luz <luzma_87@yahoo.com>
2018-10-11 07:31:21 -0500

    adds login with phone number

--- android/app/src/main/java/com/twparking/MainApplication.java
+++ android/app/src/main/java/com/twparking/MainApplication.java
@@ -6,6 +6,7 @@ import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
 import io.invertase.firebase.database.RNFirebaseDatabasePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
                     new RNFirebasePackage(),
+                    new RNFirebaseAuthPackage(),
                     new RNFirebaseDatabasePackage(),
                     new RNFirebaseAnalyticsPackage()
             );
