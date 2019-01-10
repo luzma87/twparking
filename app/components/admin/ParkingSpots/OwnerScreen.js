852f628fe0e8c4e75d19f5113ccdcfaa8ace971c
Luz <luzma_87@yahoo.com>
2019-01-10 07:37:54 -0500

    [TWPARKING-006][LU] adds owner loading logic

--- app/components/admin/ParkingSpots/OwnerScreen.js
+++ app/components/admin/ParkingSpots/OwnerScreen.js
@@ -1,13 +1,27 @@
 /* @flow */
 import React, { Component } from 'react';
+import { ScrollView, View } from 'react-native';
+import firebase from 'react-native-firebase';
+import { isEmpty } from 'lodash';
 import TWText from '../../_common/TWText/TWText';
 import TWScreenWithNavigationBar from '../../_common/TWScreenWithNavigationBar';
 import navigationHeader from '../../../navigation/NavigationStylesHelper';
 
+const noOwners = () => (
+  <View style={{
+    alignItems: 'center', justifyContent: 'center', flex: 1,
+  }}
+  >
+    <TWText i18n="screens.admin.owners.empty" weight="bold" multiline align="center" />
+  </View>
+);
+
 type Props = {
   navigation?: any
 };
-type State = {};
+type State = {
+  owners: any
+};
 
 class OwnerScreen extends Component<Props, State> {
   static navigationOptions = navigationHeader.noHeader;
@@ -16,14 +30,44 @@ class OwnerScreen extends Component<Props, State> {
     navigation: undefined,
   };
 
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      owners: [],
+    };
+    this.getOwners();
+  }
+
+  getOwners() {
+    console.warn('trying to get owners');
+    firebase.auth().onAuthStateChanged(() => {
+      firebase.database()
+        .ref('owners')
+        .once('value', (snapshot) => {
+          if (snapshot.exists()) {
+            const { owners } = this.state;
+            if (isEmpty(owners)) {
+              this.setState({ owners: snapshot.val() });
+            }
+            console.warn('done loading owners');
+          }
+        }, () => { console.warn('??'); });
+    });
+  }
+
+  ownersList() {
+
+  }
+
   render() {
     const { navigation } = this.props;
+    const { owners } = this.state;
     return (
       <TWScreenWithNavigationBar
-        onPress={() => console.warn('pepe')}
+        onPress={() => navigation.goBack()}
         i18nTitle="screens.admin.home.tabs.Owners"
       >
-        <TWText text="owners" />
+        {owners.length === 0 ? noOwners() : this.ownersList()}
       </TWScreenWithNavigationBar>
     );
   }
